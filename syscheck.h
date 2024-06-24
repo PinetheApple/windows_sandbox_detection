2b9f743fa4888d046124ab4248355c591297378c
Pine <pinespace889@gmail.com>
2024-06-24 11:18:28 +0530

    Create syscheck.h

---
+++ syscheck.h
@@ -0,0 +1,12 @@
+#pragma once
+
+bool systemChecks();
+bool hasVMProcess();
+bool hasVMDrive();
+bool hasVMUsername();
+bool hasVMHostname();
+bool hasVMMac();
+bool hasVMNetworkAdapter();
+int getNumProcessors();
+bool hasVMDllsLoaded();
+bool hasKnownFileName();
